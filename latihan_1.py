224d5cd8761f2ea182a9020d12952e9d227629b2
fathinalwan15 <153715451+fathinalwan15@users.noreply.github.com>
2023-12-21 10:33:14 +0800

    Add files via upload

---
+++ latihan_1.py
@@ -0,0 +1,7 @@
+nilai = 70
+nilai = float(input('Masukkan nilai: '))
+batas_lulus = 65
+if nilai > batas_lulus:
+    print('Selamat, kamu lulus!')
+else:
+    print('Sayang sekali, kamu tidak lulus!')
---
+++ latihan_3.py
@@ -0,0 +1,9 @@
+
+nama = input('Nama karyawan:')
+pendapatan = float(input('Pendapatan: '))
+if pendapatan >1000:
+    print('Status : Berhak')
+else:
+    print('Status : Tidak Berhak')
+
+
